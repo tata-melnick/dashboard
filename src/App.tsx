b7f125efc7854c00597ed4565a94fb127050c741
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-20 16:01:55 +0300

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css"
+import Layout from "./Layout"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-    return <div></div>
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route path="/" />
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
