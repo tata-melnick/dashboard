b66118d8aa3ec5d29c89d88683e8f1e33248c6f4
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-20 16:51:16 +0300

    Add component Header and styles [Header]

---
+++ src/Layout/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import styles from "./styles.module.css"
+
+const Header: React.FC = () => {
+    return (
+        <div className={styles.header}>
+            <div className={styles.user}>
+                <div>
+                    <div className={styles.name}>John Smith</div>
+                    <div className={styles.job}>HR-manager</div>
+                </div>
+                <div className={styles.img}>
+                    <img src="/public/avatar.png" alt="avatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
