dba19faac7c3c8f38af4cdc26aa7c02557bac238
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-25 00:36:54 +0300

    Add component Layout and styles [Layout]

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
+import { Outlet } from "react-router-dom"
+import styles from "./layout.module.css"
 
 const Layout: React.FC = () => {
     return (
-        <>
+        <div className={styles.layout}>
             <Sidebar />
-            <Header />
-        </>
+            <div className={styles.wrap}>
+                <Header />
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
