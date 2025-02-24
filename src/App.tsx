9edb7893008ec8fc788d26d16930dc644f88cac8
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-02-25 00:35:47 +0300

    Add react-router [App]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css"
 import Layout from "./Layout"
 import { Route, Routes } from "react-router-dom"
+import MainPage from "./pages/MainPage";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Layout />}>
-                <Route path="/" />
+                <Route path="/"  element={<MainPage />}/>
+                <Route path="/search"  element={<div>Поиск</div>}/>
+                <Route path="/stats"  element={<div>Статистика</div>}/>
+                <Route path="/bell"  element={<div>Уведомления</div>}/>
+                <Route path="/chart-pie-alt"  element={<div>Диаграмма</div>}/>
+                <Route path="/like"  element={<div>Избраное</div>}/>
+                <Route path="/credit-card"  element={<div>Карты</div>}/>
+                <Route path="/authorization"  element={<div>Авторизация</div>}/>
             </Route>
         </Routes>
     )
